f37d3bab4aa93a814c140bfb97dfc928cde5f620
Ryan Zeigler <zeiglerr@gmail.com>
2017-01-04 09:05:09 -0600

    Finally parse things.

--- src/Bootstrap.purs
+++
@@ -1,4 +0,0 @@
-module Bootstrap where
-
-import Node.Yargs.Applicative (flag, yarg, runY)
-import Node.Yargs.Setup (example, usage)
--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,24 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Int (fromNumber)
+import Data.Maybe (Maybe(..))
+import Data.Either (Either(..))
+import Control.Monad.Eff (Eff())
+import Control.Monad.Eff.Exception (EXCEPTION())
+import Control.Monad.Eff.Console (CONSOLE(), logShow, log)
+import Node.Yargs.Applicative (yarg, runY)
+import Node.Yargs.Setup (example, usage)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+app :: forall eff. Maybe Int -> Maybe Int -> Eff (console :: CONSOLE, err :: EXCEPTION | eff) Unit
+app (Just day) (Just sect) = do
+  logShow day
+  logShow sect
+app _ _ = log "Section and Day must both be Integers"
+
+main :: forall eff. Eff (console :: CONSOLE, err :: EXCEPTION | eff) Unit
 main = do
-  log "Hello sailor!"
+  let setup = usage "$0 -d Day -s Section 'Input File'"
+              <> example "$0 -d 1 -s 2 input.txt" "Run day 1 section 2 against input.txt"
+  runY setup $ app <$> (fromNumber <$> yarg "d" ["day"] (Just "The day") (Right "A day is required") true)
+                   <*> (fromNumber <$> yarg "s" ["section"] (Just "The section") (Left 1.0) false)
