2e8c9065e5b37cb4860023586c4cb08a34322273
Ryan Zeigler <zeiglerr@gmail.com>
2017-01-05 06:22:23 -0600

    Start parsing.

--- src/Day1.purs
+++ src/Day1.purs
@@ -1,7 +1,23 @@
 module Day1 (day) where
 
 import Prelude
-import Data.List (List)
+import Data.List (List, toUnfoldable)
+import Data.String (fromCharArray)
+import Text.Parsing.StringParser (Parser)
+import Text.Parsing.StringParser.String (anyDigit, char, many1)
+
+data Dir = L | R
+data Move = Move Dir Int
+
+dirParser :: Parser Dir
+dirParser = (char 'L' *> pure L)
+            <|> (char 'R' *> pure R)
+            
+intParser :: Parser Int
+intParser = fromCharArray . toUnfoldable <$> many1 anyDigit
+
+moveParser :: Parser Move
+moveParser = Move <$> dirParser <*> intParser
 
 part_1 :: String -> String
 part_1 = id
